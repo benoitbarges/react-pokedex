ec8acb473e1acfce06e1d8b4d60793b0d4251e15
benoit barges <benoitbarges@gmail.com>
2020-12-01 09:23:36 +0100

    create moving artwork component to refacto

---
+++ src/components/MovingArtwork.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { gsap } from 'gsap'
+
+const positionReducer = (state, action) => {
+  if (action.type === 'setPosition') {
+    return {
+      ...state,
+      positionX: action.box.left + (action.box.width * 0.5),
+      positionY: action.box.top + (action.box.height * 0.5),
+      height: action.box.height,
+      width: action.box.width
+    }
+  } else if (action.type === 'resetPosition') {
+    return {
+      ...state,
+      positionX: 0,
+      positionY: 0
+    }
+  } else {
+    throw new Error("This action type isn't supported.")
+  }
+}
+
+export default function MovingArtwork({ name, id}) {
+  const [state, dispatch] = React.useReducer(
+    positionReducer,
+    {positionX: 0, positionY: 0}
+  )
+
+  const imageRef = React.useRef()
+
+  const [hover, setHover] = React.useState(false)
+
+  const src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+
+  const handleMouseMove = (e) => {
+    const box = imageRef.current.getBoundingClientRect();
+    dispatch({type: 'setPosition', box})
+
+    let x = e.clientX - state.positionX;
+    let y = e.clientY - state.positionY;
+
+
+    let hoverArea = (hover ? 0.3 : 0.4);
+    let distance = Math.sqrt( x*x + y*y );
+    if (distance < (state.width * hoverArea)) {
+      setHover(true)
+    }
+
+    if (hover) {
+      onMouseOver(x, y)
+    }
+  }
+
+  const onMouseOver = (x, y) => {
+    gsap.to(imageRef.current,  {
+      x: x * 0.4,
+      y: y * 0.4,
+      scale: 1.15,
+      ease: 'power2.out',
+      duration: 0.2
+    });
+  }
+
+  const handleMouseOut = (e) => {
+    dispatch({type: 'resetPosition'})
+    gsap.to(imageRef.current, {
+      x: 0,
+      y: 0,
+      scale: 1,
+      ease: 'elastic.out(1.2, 0.4)',
+      duration: 0.7
+    });
+    setHover(false)
+  }
+
+  return (
+    <img
+      className='artwork artwork-lg margin-auto'
+      src={src} alt={`artwork of ${name}`}
+      onMouseMove={handleMouseMove}
+      onMouseOut={handleMouseOut}
+      ref={imageRef}
+    />
+  )
+}
--- src/components/PokemonShow.js
+++ src/components/PokemonShow.js
@@ -3,7 +3,7 @@ import queryString from 'query-string'
 import { fetchPokemon } from '../utils/api'
 import colors from '../utils/colors'
 import SimpleTabs from './SimpleTabs'
-import { gsap } from 'gsap'
+import MovingArtwork from './MovingArtwork'
 
 const pokemonReducer = (state, action) => {
   if (action.type === 'success') {
@@ -17,39 +17,18 @@ const pokemonReducer = (state, action) => {
       ...state,
       error: action.error.message
     }
-  } else if (action.type === 'setPosition') {
-    return {
-      ...state,
-      position: {
-        x: action.box.left + (action.box.width * 0.5),
-        y: action.box.top + (action.box.height * 0.5),
-        height: action.box.height,
-        width: action.box.width
-      }
-    }
-  } else if (action.type === 'resetPosition') {
-    return {
-      ...state,
-      position: {
-        x: 0,
-        y: 0
-      }
-    }
   } else {
     throw new Error("This action type isn't supported.")
   }
 }
 
 export default function PokemonShow({ location }) {
-  const imageRef = React.useRef()
-
-  const [hover, setHover] = React.useState(false)
 
   const { id } = queryString.parse(location.search)
 
   const [state, dispatch] = React.useReducer(
     pokemonReducer,
-    { pokemon: null, loading: true, error: null, position: {x: 0, y:0} }
+    { pokemon: null, loading: true, error: null }
   )
 
   React.useEffect(() => {
@@ -59,48 +38,6 @@ export default function PokemonShow({ location }) {
   }, [id])
 
   const { pokemon, loading } = state
-  const src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
-
-  const handleMouseMove = (e) => {
-    const box = imageRef.current.getBoundingClientRect();
-    dispatch({type: 'setPosition', box})
-
-    let x = e.clientX - state.position.x;
-    let y = e.clientY - state.position.x;
-
-
-    let hoverArea = (hover ? 0.3 : 0.4);
-    let distance = Math.sqrt( x*x + y*y );
-    if (distance < (state.position.width * hoverArea)) {
-      setHover(true)
-    }
-
-    if (hover) {
-      onMouseOver(x, y)
-    }
-  }
-
-  const onMouseOver = (x, y) => {
-    gsap.to(imageRef.current,  {
-      x: x * 0.4,
-      y: y * 0.4,
-      scale: 1.15,
-      ease: 'power2.out',
-      duration: 0.2
-    });
-  }
-
-  const handleMouseOut = (e) => {
-    dispatch({type: 'resetPosition'})
-    gsap.to(imageRef.current, {
-      x: 0,
-      y: 0,
-      scale: 1,
-      ease: 'elastic.out(1.2, 0.4)',
-      duration: 0.7
-    });
-    setHover(false)
-  }
 
   if (loading) {
     return <h1>Loading...</h1>
@@ -112,12 +49,9 @@ export default function PokemonShow({ location }) {
         <h1 className='pokemon-name capitalize'>
           {pokemon.name}
         </h1>
-        <img
-          className='artwork artwork-lg margin-auto'
-          src={src} alt={`artwork of ${pokemon.name}`}
-          onMouseMove={handleMouseMove}
-          onMouseOut={handleMouseOut}
-          ref={imageRef}
+        <MovingArtwork
+          name={pokemon.name}
+          id={pokemon.id}
         />
       </div>
       <div className='show-infos'>
